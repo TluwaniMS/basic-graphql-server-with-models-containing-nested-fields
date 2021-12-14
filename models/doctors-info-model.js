15146944fc7a43c41a2200b4fb1ecfb835dcc491
tluwaniMS <tluwani2020@gmail.com>
2021-12-14 18:24:12 +0200

    adding-doctors-info-model-properties

--- models/doctors-info-model.js
+++ models/doctors-info-model.js
@@ -1,5 +1,12 @@
 const { GraphQLInt, GraphQLString, GraphQLNonNull, GraphQLObjectType } = require("graphql");
 
-const doctorsInfoModel = new GraphQLObjectType({});
+const doctorsInfoModel = new GraphQLObjectType({
+  name: "DoctorsInfo",
+  fields: () => ({
+    doctorsId: { type: new GraphQLNonNull(GraphQLInt) },
+    specialty: { type: new GraphQLNonNull(GraphQLString) },
+    hospital: { type: new GraphQLNonNull(GraphQLString) }
+  })
+});
 
 module.exports = { doctorsInfoModel };
