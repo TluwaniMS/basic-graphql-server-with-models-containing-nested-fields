7ad7ab2c3373a8ece04f09f009bbe0a08b64bef2
tluwaniMS <tluwani2020@gmail.com>
2021-12-14 07:14:45 +0200

    adding-municipalities-sample-data

---
+++ sample-data/municipalities-sample-data.js
@@ -0,0 +1,9 @@
+const municipalities = [
+  { municipalityName: "Blouberg Municipality", municipalityKey: "BLBG" },
+  { municipalityName: "Capricorn Municipality", municipalityKey: "CPRN" },
+  { municipalityName: "Ekhuruleni Municipality", municipalityKey: "EKRN" },
+  { municipalityName: "Waterval Municipality", municipalityKey: "WTRV" },
+  { municipalityName: "Luonde Municipality", municipalityKey: "LNDE" }
+];
+
+module.exports = { municipalities };
