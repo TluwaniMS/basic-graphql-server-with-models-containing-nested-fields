ffdf72e2d7f1806c20961d741cfeb164fef23429
tluwaniMS <tluwani2020@gmail.com>
2021-12-14 07:13:19 +0200

    adding-hospitals-sampe-data

---
+++ sample-data/hospitals-sample-data.js
@@ -0,0 +1,69 @@
+const hospitals = [
+  {
+    hospitalName: "Sekhukhune Private Hospital",
+    hospitalKey: "SKKN",
+    municipality: "BLBG"
+  },
+  {
+    hospitalName: "Shapo Private Hospital",
+    hospitalKey: "SHPO",
+    municipality: "BLBG"
+  },
+  {
+    hospitalName: "Polokwane Hospital",
+    hospitalKey: "PLKH",
+    municipality: "EKRN"
+  },
+  {
+    hospitalName: "Botlokwa Hospital",
+    hospitalKey: "BTLK",
+    municipality: "WTRV"
+  },
+  {
+    hospitalName: "Marobjyane Private Hospital",
+    hospitalKey: "MRBJ",
+    municipality: "EKRN"
+  },
+  {
+    hospitalName: "Ntsundeni Private Hospital",
+    hospitalKey: "NTNP",
+    municipality: "LNDE"
+  },
+  {
+    hospitalName: "Moshe Hospital",
+    hospitalKey: "MSHH",
+    municipality: "WTRV"
+  },
+  {
+    hospitalName: "Marabastad Hospital",
+    hospitalKey: "MRSB",
+    municipality: "BLBG"
+  },
+  {
+    hospitalName: "Baloyi Hospital",
+    hospitalKey: "BLYH",
+    municipality: "EKRN"
+  },
+  {
+    hospitalName: "Vhatwanamba Private Health Care",
+    hospitalKey: "VTPH",
+    municipality: "CPRN"
+  },
+  {
+    hospitalName: "Bulombo Hospital",
+    hospitalKey: "BLMH",
+    municipality: "LNDE"
+  },
+  {
+    hospitalName: "Mashashane Hospital",
+    hospitalKey: "MSSH",
+    municipality: "WTRV"
+  },
+  {
+    hospitalName: "dendron Health Centre",
+    hospitalKey: "DNDH",
+    municipality: "CPRN"
+  }
+];
+
+module.exports = { hospitals };
