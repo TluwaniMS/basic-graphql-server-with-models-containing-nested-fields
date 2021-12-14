8707924a29f1217e2feda5427381dbedfd7f1a64
tluwaniMS <tluwani2020@gmail.com>
2021-12-14 07:03:01 +0200

    adding-doctors-sample-data

---
+++ sample-data/doctors-sample-data.js
@@ -0,0 +1,254 @@
+const doctors = [
+  {
+    id: 1,
+    firstName: "Dean",
+    lastName: "Hill",
+    gender: "Male",
+    password: "dean1234",
+    email: "dean@mock.com",
+    hospital: "SKKN",
+    specialty: "PDTN"
+  },
+  {
+    id: 2,
+    firstName: "Lufuno",
+    lastName: "Sekgothe",
+    gender: "Female",
+    password: "lufu1234",
+    email: "lufuno@mock.com",
+    hospital: "PLKH",
+    specialty: "CDGT"
+  },
+  {
+    id: 3,
+    firstName: "Refilwe",
+    lastName: "Phago",
+    gender: "Female",
+    password: "refi1234",
+    email: "refilwe@mock.com",
+    hospital: "DNDH",
+    specialty: "URLG"
+  },
+  {
+    id: 4,
+    firstName: "Seba",
+    lastName: "Sibika",
+    gender: "Female",
+    password: "seba1234",
+    email: "seba@mock.com",
+    hospital: "DNDH",
+    specialty: "CDGT"
+  },
+  {
+    id: 5,
+    firstName: "Katlego",
+    lastName: "Mamani",
+    gender: "Female",
+    password: "katl1234",
+    email: "katlego@mock.com",
+    hospital: "SKKN",
+    specialty: "PDTN"
+  },
+  {
+    id: 6,
+    firstName: "Idah",
+    lastName: "Leseme",
+    gender: "Female",
+    password: "idah1234",
+    email: "idah@mock.com",
+    hospital: "MSSH",
+    specialty: "PDTN"
+  },
+  {
+    id: 7,
+    firstName: "Jessica",
+    lastName: "Shadung",
+    gender: "Female",
+    password: "jess1234",
+    email: "jessica@mock.com",
+    hospital: "DNDH",
+    specialty: "DMTG"
+  },
+  {
+    id: 8,
+    firstName: "Tshego",
+    lastName: "Seoka",
+    gender: "Male",
+    password: "tshe1234",
+    email: "tshego@mock.com",
+    hospital: "SKKN",
+    specialty: "CDGT"
+  },
+  {
+    id: 9,
+    firstName: "Jethro",
+    lastName: "Mokgohloa",
+    gender: "Male",
+    password: "jeth1234",
+    email: "jethro@mock.com",
+    hospital: "PLKH",
+    specialty: "URLG"
+  },
+  {
+    id: 10,
+    firstName: "Kgaogelo",
+    lastName: "Munyangane",
+    gender: "Female",
+    password: "kgao1234",
+    email: "kgaogelo@mock.com",
+    hospital: "MSSH",
+    specialty: "DMTG"
+  },
+  {
+    id: 11,
+    firstName: "Mmadi",
+    lastName: "Ndlovu",
+    gender: "Male",
+    password: "mmad1234",
+    email: "mmadi@mock.com",
+    hospital: "BTLK",
+    specialty: "DMTG"
+  },
+  {
+    id: 12,
+    firstName: "Abigail",
+    lastName: "Sekhosana",
+    gender: "Female",
+    password: "abig1234",
+    email: "abigail@mock.com",
+    hospital: "NTNP",
+    specialty: "URLG"
+  },
+  {
+    id: 13,
+    firstName: "Mmakosha",
+    lastName: "Deagrela",
+    gender: "Female",
+    password: "mmak1234",
+    email: "mmakosha@mock.com",
+    hospital: "SHPO",
+    specialty: "URLG"
+  },
+  {
+    id: 14,
+    firstName: "Dorcus",
+    lastName: "Maruma",
+    gender: "Female",
+    password: "dorc1234",
+    email: "dorcus@mock.com",
+    hospital: "MRBJ",
+    specialty: "DMTG"
+  },
+  {
+    id: 15,
+    firstName: "Denzel",
+    lastName: "Mophosho",
+    gender: "Male",
+    password: "denz1234",
+    email: "denzel@mock.com",
+    hospital: "NTNP",
+    specialty: "NRLG"
+  },
+  {
+    id: 16,
+    firstName: "Lerato",
+    lastName: "Sehume",
+    gender: "Female",
+    password: "lera1234",
+    email: "lerato@mock.com",
+    hospital: "SHPO",
+    specialty: "RDLG"
+  },
+  {
+    id: 17,
+    firstName: "Londokuhle",
+    lastName: "Ndala",
+    gender: "Male",
+    password: "lond1234",
+    email: "londokuhle@mock.com",
+    hospital: "BLYH",
+    specialty: "CDGT"
+  },
+  {
+    id: 18,
+    firstName: "Thovhedzo",
+    lastName: "Maosa",
+    gender: "Male",
+    password: "thov1234",
+    email: "thovhedzo@mock.com",
+    hospital: "SHPO",
+    specialty: "RDLG"
+  },
+  {
+    id: 19,
+    firstName: "Reneilwe",
+    lastName: "Dikana",
+    gender: "Female",
+    password: "rene1234",
+    email: "reneilwe@mock.com",
+    hospital: "MRSB",
+    specialty: "NRLG"
+  },
+  {
+    id: 20,
+    firstName: "Philicia",
+    lastName: "Sothobe",
+    gender: "Female",
+    password: "phil1234",
+    email: "philicia@mock.com",
+    hospital: "BLMH",
+    specialty: "PSCT"
+  },
+  {
+    id: 21,
+    firstName: "Eddy",
+    lastName: "Manaka",
+    gender: "Male",
+    password: "eddy1234",
+    email: "eddy@mock.com",
+    hospital: "SHPO",
+    specialty: "RDLG"
+  },
+  {
+    id: 22,
+    firstName: "Mathibela",
+    lastName: "Serumula",
+    gender: "Male",
+    password: "math1234",
+    email: "mathibela@mock.com",
+    hospital: "BLYH",
+    specialty: "NRLG"
+  },
+  {
+    id: 23,
+    firstName: "Mpho",
+    lastName: "Leshabane",
+    gender: "Male",
+    password: "mpho1234",
+    email: "mpho@mock.com",
+    hospital: "BLMH",
+    specialty: "PSCT"
+  },
+  {
+    id: 24,
+    firstName: "Lehlohonolo",
+    lastName: "Sebata",
+    gender: "Male",
+    password: "lehl1234",
+    email: "lehlohonolo@mock.com",
+    hospital: "VTPH",
+    specialty: "RDLG"
+  },
+  {
+    id: 25,
+    firstName: "Mpumi",
+    lastName: "Khoza",
+    gender: "Male",
+    password: "mpum1234",
+    email: "mpumi@mock.com",
+    hospital: "VTPH",
+    specialty: "PSCT"
+  }
+];
+
+module.exports = { doctors };
