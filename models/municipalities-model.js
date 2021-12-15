443a14c659162fb1a5919e829bca67597b2db598
tluwaniMS <tluwani2020@gmail.com>
2021-12-15 11:51:18 +0200

    adding-logic-to-get-hospitals-linked-to-municipality

--- models/municipalities-model.js
+++ models/municipalities-model.js
@@ -1,12 +1,18 @@
 const { GraphQLList, GraphQLNonNull, GraphQLString, GraphQLObjectType } = require("graphql");
 const { HospitalsModel } = require("./hospitals-model");
+const { getHospitalsLinkedToMunicipality } = require("../services/municipality-services");
 
 const MunicipalitiesModel = new GraphQLObjectType({
   name: "Municipality",
   fields: () => ({
     municipalityName: { type: new GraphQLNonNull(GraphQLString) },
     municipalityKey: { type: new GraphQLNonNull(GraphQLString) },
-    hospitals: { type: new GraphQLList(HospitalsModel) }
+    hospitals: {
+      type: new GraphQLList(HospitalsModel),
+      resolve(parent, args) {
+        return getHospitalsLinkedToMunicipality(parent.municipalityKey);
+      }
+    }
   })
 });
 
