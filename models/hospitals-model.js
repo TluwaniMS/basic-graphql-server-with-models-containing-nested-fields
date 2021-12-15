56c978c3043282dc0174b091610e38b49e97c603
tluwaniMS <tluwani2020@gmail.com>
2021-12-15 11:44:38 +0200

    adding-logic-to-add-doctors-linked-to-hospital

--- models/hospitals-model.js
+++ models/hospitals-model.js
@@ -1,5 +1,6 @@
 const { GraphQLList, GraphQLNonNull, GraphQLString, GraphQLObjectType } = require("graphql");
 const { DoctorsModel } = require("./doctors-model");
+const { getDoctorsLinkedToHospital } = require("../services/hospital-services");
 
 const HospitalsModel = new GraphQLObjectType({
   name: "Hospital",
@@ -7,7 +8,12 @@ const HospitalsModel = new GraphQLObjectType({
     hospitalName: { type: new GraphQLNonNull(GraphQLString) },
     hospitalKey: { type: new GraphQLNonNull(GraphQLString) },
     municipality: { type: new GraphQLNonNull(GraphQLString) },
-    doctors: { type: new GraphQLList(DoctorsModel) }
+    doctors: {
+      type: new GraphQLList(DoctorsModel),
+      resolve(parent, args) {
+        return getDoctorsLinkedToHospital(parent.hospitalKey);
+      }
+    }
   })
 });
 
