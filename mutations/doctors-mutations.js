f9d28be2b74cec759d12c96489d53748ba49afa7
tluwaniMS <tluwani2020@gmail.com>
2021-12-21 10:02:37 +0200

    final-touches-to-server-logic

--- mutations/doctors-mutations.js
+++ mutations/doctors-mutations.js
@@ -2,9 +2,9 @@ const { GraphQLString, GraphQLInt, GraphQLNonNull } = require("graphql");
 const { deleteDoctorById } = require("../services/doctor-services");
 
 const deleteDoctorMutation = {
-  type: new GraphQLNonNull(GraphQLString),
+  type: GraphQLString,
   args: {
-    doctorsId: { type: new GraphQLNonNull(GraphQLString) }
+    doctorsId: { type: new GraphQLNonNull(GraphQLInt) }
   },
   resolve(parent, args) {
     return deleteDoctorById(args);
