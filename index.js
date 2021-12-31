4b38d9dfd4a01503e6d9b3e5eed8b1e63bfd31b3
tluwaniMS <tluwani2020@gmail.com>
2021-12-31 10:24:35 +0200

    adding-cors-configuration-and-changing-server-port

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 const { graphqlHTTP } = require("express-graphql");
 const schema = require("./main-graph-schema");
 const app = express();
 
+app.use(cors());
 app.use("/graphql", graphqlHTTP({ schema: schema, graphiql: true }));
 
 module.exports = { app };
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const { app } = require("./index");
-const PORT = process.env.PORT | 3000;
+const PORT = process.env.PORT | 5000;
 
 app.listen(PORT, () => {
   console.log(`server is running on port: ${PORT}`);
