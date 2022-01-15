0cfb96b11b4dc1d76385c03bacb4fe87d9994728
tluwaniMS <tluwani2020@gmail.com>
2022-01-15 10:50:26 +0200

    adding-dotenv-package-and-configuration

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const dotenv = require("dotenv");
 const cors = require("cors");
 const { graphqlHTTP } = require("express-graphql");
 const schema = require("./main-graph-schema");
 const app = express();
 
+dotenv.config();
 app.use(cors());
 app.use("/graphql", graphqlHTTP({ schema: schema, graphiql: true }));
 
