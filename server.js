68d22b275e665000095502a62479a2b6709295fa
tluwaniMS <tluwani2020@gmail.com>
2022-01-15 17:37:00 +0200

    removing-logic-to-connect-to-mongo-database

--- server.js
+++ server.js
@@ -7,6 +7,6 @@ app.listen(PORT, () => {
   console.log(`server is running on port: ${PORT}`);
 });
 
-(async () => {
-  await connectToDataBase();
-})();
+// (async () => {
+//   await connectToDataBase();
+// })();
