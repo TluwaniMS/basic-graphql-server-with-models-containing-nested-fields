e9eee4b343f635dedfc2214c4e809b7e4ff0f42f
tluwaniMS <tluwani2020@gmail.com>
2022-01-15 11:34:23 +0200

    adding-database-connection-configuration

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const { app } = require("./index");
+const { connectToDataBase } = require("./data-base-configuration");
+
 const PORT = process.env.PORT | 5000;
 
 app.listen(PORT, () => {
   console.log(`server is running on port: ${PORT}`);
 });
+
+(async () => {
+  await connectToDataBase();
+})();
