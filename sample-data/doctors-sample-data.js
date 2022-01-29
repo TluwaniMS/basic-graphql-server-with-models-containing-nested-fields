e4d5209e17e9151b2036f90b4704b2495d8fe9fc
tluwaniMS <tluwani2020@gmail.com>
2022-01-29 22:39:40 +0200

    adding-mongo-ids-to-sample-data

--- sample-data/doctors-sample-data.js
+++ sample-data/doctors-sample-data.js
@@ -1,203 +1,303 @@
 const Doctors = [
   {
-    id: 1,
+    id: "61f58a1d2f87a0e538236b25",
     firstName: "Dean",
     lastName: "Hill",
     gender: "Male",
     email: "dean@mock.com",
-    hospital: "SKKN"
+    hospital: "SKKN",
+    info: {
+      specialty: "Pediatrician",
+      hospital: "Sekhukhune Private Hospital"
+    }
   },
   {
-    id: 2,
+    id: "61f58a1d2f87a0e538236b26",
     firstName: "Lufuno",
     lastName: "Sekgothe",
     gender: "Female",
     email: "lufuno@mock.com",
-    hospital: "PLKH"
+    hospital: "PLKH",
+    info: {
+      specialty: "Cardiologist",
+      hospital: "Polokwane Hospital"
+    }
   },
   {
-    id: 3,
+    id: "61f58a1d2f87a0e538236b27",
     firstName: "Refilwe",
     lastName: "Phago",
     gender: "Female",
     email: "refilwe@mock.com",
-    hospital: "DNDH"
+    hospital: "DNDH",
+    info: {
+      specialty: "Urologist",
+      hospital: "dendron Health Centre"
+    }
   },
   {
-    id: 4,
+    id: "61f58a1d2f87a0e538236b28",
     firstName: "Seba",
     lastName: "Sibika",
     gender: "Female",
     email: "seba@mock.com",
-    hospital: "DNDH"
+    hospital: "DNDH",
+    info: {
+      specialty: "Cardiologist",
+      hospital: "dendron Health Centre"
+    }
   },
   {
-    id: 5,
+    id: "61f58a1d2f87a0e538236b29",
     firstName: "Katlego",
     lastName: "Mamani",
     gender: "Female",
     email: "katlego@mock.com",
-    hospital: "SKKN"
+    hospital: "SKKN",
+    info: {
+      specialty: "Pediatrician",
+      hospital: "Sekhukhune Private Hospital"
+    }
   },
   {
-    id: 6,
+    id: "61f58a1d2f87a0e538236b2a",
     firstName: "Idah",
     lastName: "Leseme",
     gender: "Female",
     email: "idah@mock.com",
-    hospital: "MSSH"
+    hospital: "MSSH",
+    info: {
+      specialty: "Pediatrician",
+      hospital: "Mashashane Hospital"
+    }
   },
   {
-    id: 7,
+    id: "61f58a1d2f87a0e538236b2b",
     firstName: "Jessica",
     lastName: "Shadung",
     gender: "Female",
     email: "jessica@mock.com",
-    hospital: "DNDH"
+    hospital: "DNDH",
+    info: {
+      specialty: "Dematologist",
+      hospital: "dendron Health Centre"
+    }
   },
   {
-    id: 8,
+    id: "61f58a1d2f87a0e538236b2c",
     firstName: "Tshego",
     lastName: "Seoka",
     gender: "Male",
     email: "tshego@mock.com",
-    hospital: "SKKN"
+    hospital: "SKKN",
+    info: {
+      specialty: "Cardiologist",
+      hospital: "Sekhukhune Private Hospital"
+    }
   },
   {
-    id: 9,
+    id: "61f58a1d2f87a0e538236b2d",
     firstName: "Jethro",
     lastName: "Mokgohloa",
     gender: "Male",
     email: "jethro@mock.com",
-    hospital: "PLKH"
+    hospital: "PLKH",
+    info: {
+      specialty: "Urologist",
+      hospital: "Polokwane Hospital"
+    }
   },
   {
-    id: 10,
+    id: "61f58a1d2f87a0e538236b2e",
     firstName: "Kgaogelo",
     lastName: "Munyangane",
     gender: "Female",
     email: "kgaogelo@mock.com",
-    hospital: "MSSH"
+    hospital: "MSSH",
+    info: {
+      specialty: "Dematologist",
+      hospital: "Mashashane Hospital"
+    }
   },
   {
-    id: 11,
+    id: "61f58abe03175729b2ffb237",
     firstName: "Mmadi",
     lastName: "Ndlovu",
     gender: "Male",
     email: "mmadi@mock.com",
-    hospital: "BTLK"
+    hospital: "BTLK",
+    info: {
+      specialty: "Dematologist",
+      hospital: "Botlokwa Hospital"
+    }
   },
   {
-    id: 12,
+    id: "61f58abe03175729b2ffb238",
     firstName: "Abigail",
     lastName: "Sekhosana",
     gender: "Female",
     email: "abigail@mock.com",
-    hospital: "NTNP"
+    hospital: "NTNP",
+    info: {
+      specialty: "Urologist",
+      hospital: "Ntsundeni Private Hospital"
+    }
   },
   {
-    id: 13,
+    id: "61f58abe03175729b2ffb239",
     firstName: "Mmakosha",
     lastName: "Deagrela",
     gender: "Female",
     email: "mmakosha@mock.com",
-    hospital: "SHPO"
+    hospital: "SHPO",
+    info: {
+      specialty: "Urologist",
+      hospital: "Shapo Private Hospital"
+    }
   },
   {
-    id: 14,
+    id: "61f58abe03175729b2ffb23a",
     firstName: "Dorcus",
     lastName: "Maruma",
     gender: "Female",
     email: "dorcus@mock.com",
-    hospital: "MRBJ"
+    hospital: "MRBJ",
+    info: {
+      specialty: "Dematologist",
+      hospital: "Marobjyane Private Hospital"
+    }
   },
   {
-    id: 15,
+    id: "61f58abe03175729b2ffb23b",
     firstName: "Denzel",
     lastName: "Mophosho",
     gender: "Male",
     email: "denzel@mock.com",
-    hospital: "NTNP"
+    hospital: "NTNP",
+    info: {
+      specialty: "Neurologist",
+      hospital: "Ntsundeni Private Hospital"
+    }
   },
   {
-    id: 16,
+    id: "61f58abe03175729b2ffb23c",
     firstName: "Lerato",
     lastName: "Sehume",
     gender: "Female",
     email: "lerato@mock.com",
-    hospital: "SHPO"
+    hospital: "SHPO",
+    info: {
+      specialty: "Radiologist",
+      hospital: "Shapo Private Hospital"
+    }
   },
   {
-    id: 17,
+    id: "61f58abe03175729b2ffb23d",
     firstName: "Londokuhle",
     lastName: "Ndala",
     gender: "Male",
     email: "londokuhle@mock.com",
-    hospital: "BLYH"
+    hospital: "BLYH",
+    info: {
+      specialty: "Cardiologist",
+      hospital: "Baloyi Hospital"
+    }
   },
   {
-    id: 18,
+    id: "61f58abe03175729b2ffb23e",
     firstName: "Thovhedzo",
     lastName: "Maosa",
     gender: "Male",
     email: "thovhedzo@mock.com",
-    hospital: "SHPO"
+    hospital: "SHPO",
+    info: {
+      specialty: "Radiologist",
+      hospital: "Shapo Private Hospital"
+    }
   },
   {
-    id: 19,
+    id: "61f58abe03175729b2ffb23f",
     firstName: "Reneilwe",
     lastName: "Dikana",
     gender: "Female",
     email: "reneilwe@mock.com",
-    hospital: "MRSB"
+    hospital: "MRSB",
+    info: {
+      specialty: "Neurologist",
+      hospital: "Marabastad Hospital"
+    }
   },
   {
-    id: 20,
+    id: "61f58abe03175729b2ffb240",
     firstName: "Philicia",
     lastName: "Sothobe",
     gender: "Female",
     email: "philicia@mock.com",
-    hospital: "BLMH"
+    hospital: "BLMH",
+    info: {
+      specialty: "Psychiatrist",
+      hospital: "Bulombo Hospital"
+    }
   },
   {
-    id: 21,
+    id: "61f58b1d7721a4ec976894e3",
     firstName: "Eddy",
     lastName: "Manaka",
     gender: "Male",
     email: "eddy@mock.com",
-    hospital: "SHPO"
+    hospital: "SHPO",
+    info: {
+      specialty: "Radiologist",
+      hospital: "Shapo Private Hospital"
+    }
   },
   {
-    id: 22,
+    id: "61f58b1d7721a4ec976894e4",
     firstName: "Mathibela",
     lastName: "Serumula",
     gender: "Male",
     email: "mathibela@mock.com",
-    hospital: "BLYH"
+    hospital: "BLYH",
+    info: {
+      specialty: "Neurologist",
+      hospital: "Baloyi Hospital"
+    }
   },
   {
-    id: 23,
+    id: "61f58b1d7721a4ec976894e5",
     firstName: "Mpho",
     lastName: "Leshabane",
     gender: "Male",
     email: "mpho@mock.com",
-    hospital: "BLMH"
+    hospital: "BLMH",
+    info: {
+      specialty: "Psychiatrist",
+      hospital: "Bulombo Hospital"
+    }
   },
   {
-    id: 24,
+    id: "61f58b1d7721a4ec976894e6",
     firstName: "Lehlohonolo",
     lastName: "Sebata",
     gender: "Male",
     email: "lehlohonolo@mock.com",
-    hospital: "VTPH"
+    hospital: "VTPH",
+    info: {
+      specialty: "Radiologist",
+      hospital: "Vhatwanamba Private Health Care"
+    }
   },
   {
-    id: 25,
+    id: "61f58b1d7721a4ec976894e7",
     firstName: "Mpumi",
     lastName: "Khoza",
     gender: "Male",
     email: "mpumi@mock.com",
-    hospital: "VTPH"
+    hospital: "VTPH",
+    info: {
+      specialty: "Psychiatrist",
+      hospital: "Vhatwanamba Private Health Care"
+    }
   }
 ];
 
--- sample-data/municipalities-sample-data.js
+++ sample-data/municipalities-sample-data.js
@@ -1,9 +1,9 @@
 const Municipalities = [
-  { municipalityName: "Blouberg Municipality", municipalityKey: "BLBG" },
-  { municipalityName: "Capricorn Municipality", municipalityKey: "CPRN" },
-  { municipalityName: "Ekhuruleni Municipality", municipalityKey: "EKRN" },
-  { municipalityName: "Waterval Municipality", municipalityKey: "WTRV" },
-  { municipalityName: "Luonde Municipality", municipalityKey: "LNDE" }
+  { id: "61f5a2ff26b7dc2be3dc90bb", municipalityName: "Blouberg Municipality", municipalityKey: "BLBG" },
+  { id: "61f5a2ff26b7dc2be3dc90bc", municipalityName: "Capricorn Municipality", municipalityKey: "CPRN" },
+  { id: "61f5a2ff26b7dc2be3dc90bd", municipalityName: "Ekhuruleni Municipality", municipalityKey: "EKRN" },
+  { id: "61f5a2ff26b7dc2be3dc90be", municipalityName: "Waterval Municipality", municipalityKey: "WTRV" },
+  { id: "61f5a2ff26b7dc2be3dc90bf", municipalityName: "Luonde Municipality", municipalityKey: "LNDE" }
 ];
 
 module.exports = { Municipalities };
