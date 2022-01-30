30f48ed3712e841adf7398602b1277bd2cfcac98
tluwaniMS <tluwani2020@gmail.com>
2022-01-30 11:43:34 +0200

    adding-municipality-migration-script-service

--- migration-script-services/municipalities-script-services.js
+++ migration-script-services/municipalities-script-services.js
@@ -1,12 +1,50 @@
 const { Hospitals } = require("../sample-data/hospitals-sample-data");
+const { Municipalities } = require("../sample-data/municipalities-sample-data");
 
-const extractHospitalsLinkedToMunicipalities = () => {
-  const sampleHospitals = Hospitals();
-  const hospitalsLinkedToMunicipality = sampleHospitals.filter((hospital) => hospital.municipality === municipalityKey);
+const mainFunctionForLinkingHospitalsAndMunicipalityIdsThatAreRelated = () => {
+  const hospitals = Hospitals();
+  const municipalities = Municipalities();
+  const arrayWithObjectContainingLinkedMunicipalityAndHospitalIds = [];
+
+  municipalities.forEach((municipality) => {
+    const hospitalsLinkedToMunicipality = extractHospitalsLinkedToMunicipalities(
+      municipality.municipalityKey,
+      hospitals
+    );
+    const hospitalIds = extractHospitalIdsOfHospitalsLinkedToMunicipality(hospitalsLinkedToMunicipality);
+    const objectWithLinkedMunicipalityAndHospitalIds = createObjectWithLinkedHospitalAndMunicipalityIds(
+      municipality._id,
+      hospitalIds
+    );
+
+    arrayWithObjectContainingLinkedMunicipalityAndHospitalIds.push(...objectWithLinkedMunicipalityAndHospitalIds);
+  });
+
+  return arrayWithObjectContainingLinkedMunicipalityAndHospitalIds;
+};
+
+const extractHospitalsLinkedToMunicipalities = (municipalityKey, hospitals) => {
+  const hospitalsLinkedToMunicipality = hospitals.filter((hospital) => hospital.municipality === municipalityKey);
 
   return hospitalsLinkedToMunicipality;
 };
 
+const extractHospitalIdsOfHospitalsLinkedToMunicipality = (hospitalsLinkedToMunicipality) => {
+  return hospitalsLinkedToMunicipality.map((hospital) => hospital._id);
+};
+
+const createObjectWithLinkedHospitalAndMunicipalityIds = (municipalityId, arrayWithHospitalIds) => {
+  const arrayWithLinkedHospitalAndMunicipalityIds = [];
+
+  arrayWithHospitalIds.forEach((id) => {
+    const objectWithLinkedIds = { municipalityId: municipalityId, hospitalId: id };
+
+    arrayWithLinkedHospitalAndMunicipalityIds.push(objectWithLinkedIds);
+  });
+
+  return arrayWithLinkedHospitalAndMunicipalityIds;
+};
+
 module.exports = {
-  extractHospitalsLinkedToMunicipalities
+  mainFunctionForLinkingHospitalsAndMunicipalityIdsThatAreRelated
 };
